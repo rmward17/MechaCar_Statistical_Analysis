4bcf54f293f4b9aa52570f8f8841867af3f925ab
rmward17 <wardrachelm17@gmail.com>
2022-06-04 12:17:33 -0400

    t-tests

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,19 @@ coil <- read.csv("Suspension_Coil.csv") #read in coil data
 total_summary <- coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI)^2, SD=sd(PSI)) #summary table
 lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI)^2, SD=sd(PSI)) #lot summary table
 
+# Deliverable 3
+
+#t-test for true populaton mean
+t.test(coil$PSI,mu=1500)
+
+#Lot 1 t-test
+Lot1 = subset(coil, Manufacturing_Lot == "Lot1")
+t.test(Lot1$PSI,mu=1500)
+
+#Lot 2 t-test
+Lot2 = subset(coil, Manufacturing_Lot == "Lot2")
+t.test(Lot2$PSI,mu=1500)
+
+#Lot 3 t-test
+Lot3 = subset(coil, Manufacturing_Lot == "Lot3")
+t.test(Lot3$PSI,mu=1500)
