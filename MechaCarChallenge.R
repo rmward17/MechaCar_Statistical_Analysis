9734c24dc62509c795c80411e4bb1d84b04c910b
rmward17 <wardrachelm17@gmail.com>
2022-06-04 11:54:22 -0400

    Suspension Coil Analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,13 @@
+#Deliverable 1
+
 library(dplyr) # load the dplyr package
-MechaCar <- read.csv("MechaCar_mpg.csv") #read in mpg datafile
+MechaCar <- read.csv("MechaCar_mpg.csv") #read in mpg data
 lm(mpg ~ vehicle_length+ vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar) #linear regression
 summary(lm(mpg ~ vehicle_length+ vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)) #get p-value and r-sqd for linear regression model
+
+# Deliverable 2
+
+coil <- read.csv("Suspension_Coil.csv") #read in coil data
+total_summary <- coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI)^2, SD=sd(PSI)) #summary table
+lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI)^2, SD=sd(PSI)) #lot summary table
+
